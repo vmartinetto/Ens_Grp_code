70bbbdbac0b16e7c8a52b0caa0d005e4c1c99ff7
sarasbbg <79868042+sarasbbg@users.noreply.github.com>
2021-03-08 13:09:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,3 @@ import math
 from scipy.linalg import norm
 import scipy as sp
 from numpy.linalg import inv
-
-# Define box size
-L = 10
-# Define number of GridPoints
-Nx = 100
-# Define the grid
-x = np.linspace(-L/2, L/2, Nx, endpoint = False) 
-# This reads "I want Nx points equally distributed"..
