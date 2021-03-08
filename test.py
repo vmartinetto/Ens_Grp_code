2e3da144b6a90fe34c10a85e0255bec8d96dc4b0
sarasbbg <sgiarrusso@ucmerced.edu>
2021-03-08 17:48:58 +0100

    building the matrix of zeros

--- test.py
+++ test.py
@@ -6,3 +6,18 @@ import scipy as sp
 from numpy.linalg import inv
 Lx=10
 Nx=100
+# Define the grid
+x = np.linspace(-Lx/2, Lx/2, Nx, endpoint = False)
+# This reads "I want Nx points equally distributed"
+
+# Define dx
+dx =x[2] - x[1] #int(L/Nx) #this gives zero division error
+
+# Define normalizing prefactor wavefunction
+Norm = 1/np.sqrt(dx)
+# 1) Define the size
+s = (x.size,x.size)
+
+# 2) Define the x.size X x.size matrix
+h = np.zeros(s)
+print(h)
