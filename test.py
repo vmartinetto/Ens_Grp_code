4ce0fb2cefab929045c069a911d6decd45379b13
sarasbbg <sgiarrusso@ucmerced.edu>
2021-03-11 11:16:21 +0100

    1-body kinetic energy operator matrix

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import math
 from scipy.linalg import norm
 import scipy as sp
 from numpy.linalg import inv
-Lx=10
-Nx=100
+Lx=1
+Nx=1000
 # Define the grid
 x = np.linspace(-Lx/2, Lx/2, Nx, endpoint = False)
 # This reads "I want Nx points equally distributed"
@@ -20,4 +20,10 @@ s = (x.size,x.size)
 
 # 2) Define the x.size X x.size matrix
 h = np.zeros(s)
-print(h)
+
+# Element minor diagonal
+vec = np.ones(x.size - 1)/(-2*dx**2)
+
+h = np.diag(1/(dx**2)) + np.diag(vec, k = 1) + np.diag(vec, k = -1)
+#########################################################################
+
