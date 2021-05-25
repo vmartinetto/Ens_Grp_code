6a94c199a77ab3fcca7815fb956c8fe3ec1ecb26
sarasbbg <sgiarrusso@ucmerced.edu>
2021-05-25 13:21:34 -0700

    Interaction matrix included

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from scipy.linalg import norm
 import scipy as sp
 from numpy.linalg import inv
 Lx=1
-Nx=1000
+Nx=100
 # Define the grid
 x = np.linspace(-Lx/2, Lx/2, Nx, endpoint = False)
 # This reads "I want Nx points equally distributed"
@@ -13,17 +13,44 @@ x = np.linspace(-Lx/2, Lx/2, Nx, endpoint = False)
 # Define dx
 dx =x[2] - x[1] #int(L/Nx) #this gives zero division error
 
+#Define softening parameter
+#a=0.01#in agreement with parameter in PYTB14, see eq (67)
+a=0.1
 # Define normalizing prefactor wavefunction
 Norm = 1/np.sqrt(dx)
 # 1) Define the size
 s = (x.size,x.size)
 
 # 2) Define the x.size X x.size matrix
-h = np.zeros(s)
+vext=0*np.square(x)
 
 # Element minor diagonal
 vec = np.ones(x.size - 1)/(-2*dx**2)
 
-h = np.diag(1/(dx**2)) + np.diag(vec, k = 1) + np.diag(vec, k = -1)
+# kinetic energy operator matrix
+Tmat1 = np.diag(vext+1/(dx**2)) + np.diag(vec, k = 1) + np.diag(vec, k = -1)
+Tmat2=np.kron(Tmat1,np.identity(Nx))+np.kron(np.identity(Nx),Tmat1)
+
+# interaction energy operator matrix
+rootdist=[]
+for i in range(Nx):
+    for j in range(Nx):
+        rootdist.append(abs(i-j))
+Wmat=np.zeros((Nx**2,Nx**2))
+for i in range(Nx**2):
+    Wmat[i,i]=1/math.sqrt(dx**2*(rootdist[i])**2+a**2)
+    
+# Hamiltonian operator matrix
+ham=Tmat2 + Wmat
+
+#Diagonalization
+vals,vecs = np.linalg.eigh(ham)
+
+#Check results
+e0 = vals[0]
+print('Groundstate energy:  ',e0)
+e1= vals[1]
+print('excited singlet state energy: ', e1)
+
 #########################################################################
 
