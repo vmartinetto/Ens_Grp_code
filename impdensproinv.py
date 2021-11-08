5c8c8dd281c26101887249b041aa91228130097b
Vincent Martinetto <vmartinetto@ucmerced.edu>
2021-11-07 18:48:22 -0800

    cleaned solver and added a 1000 point bi ensemble evecs

--- impdensproinv.py
+++ impdensproinv.py
@@ -7,24 +7,82 @@ from scipy.linalg import norm
 import scipy as sp
 from numpy.linalg import inv
 from scipy import interpolate
+
+#########################Define Soft-Coulomb matrix#################################
+
+
+def interaction(var):
+    return 1/np.sqrt(a**2+var**2)
+
+def SC2mat(Nx):
+    SC2intmat = np.empty((Nx,Nx))
+    for i in range(Nx):
+        for j in range(Nx):
+            SC2intmat[i,j]=interaction(x[i]-x[j])
+    return SC2intmat
+
+
+#########################Inversion Defenition#######################################
+
+
+def Lee_Bar_Inv_Ens(vinv,gdens,density,w,conv):
+    number = np.Inf
+    while  number > conv:
+        vinv = gdens*vinv/density
+        diags = np.array([vinv+diag, diag/-2, diag/-2])
+        HSguess= spa.dia_matrix((diags,[0,-1,1]),shape=s)
+        valsg, vecsg = eigsh(HSguess,which='SA')
+        gdens = (2-w)*np.square(vecsg[:,0])+w*np.square(vecsg[:,1])
+        number=(np.abs(np.ones(Nx)-gdens/density)).max()
+        print(number)
+    return vinv,valsg,vecsg
+
+
+#########################Define Variables###########################################
+
+
+# define the size, number of points, and the 1D grid
+
 Lx=1
-Nx=300
-# Define the grid
+Nx=1000
 x = np.linspace(-Lx*0.5, Lx*0.5, Nx, endpoint = False)
-# This reads "I want Nx points equally distributed"
+
 # Define dx
+
 dx =x[2] - x[1] #int(L/Nx) #this gives zero division error
+
 #Define softening parameter
+
 a=0.1
+
+# define the size of a matrix [Nx,Nx]
+
+s = (Nx,Nx)
+
+
+########################Import Data#################################################
+
+
 #Import density for N=300 and x = np.linspace(-Lx*0.5, Lx*0.5, Nx, endpoint = True)
-density1=np.genfromtxt('denspy1-300-9.dat')
-densityGS=np.genfromtxt('denspy-300-9.dat')
+density1=np.genfromtxt('denspy-1000-1-9.dat')
+densityGS=np.genfromtxt('denspy-1000-9.dat')
+
+
+########################Calculate Interacting ensemble density######################
+
+
 gw=0.125
 density= (1-gw)*densityGS + gw*density1
+
+########################Interpolation?##############################################
+
+
 #Interpolating density
 #ximp = np.linspace(-Lx*0.5, Lx*0.5, 1000, endpoint = False)
 #densweights = interpolate.splrep(ximp, densityGS, s=0)
 #densityGS = interpolate.splev(x, densweights, der=0)
+
+
 #
 #sqrt_dens0 = np.sqrt(densityGS)
 # KS potential from analytical inversion
@@ -34,74 +92,57 @@ density= (1-gw)*densityGS + gw*density1
 #diag = np.ones(1000)/dximp**2
 #Tmat = np.diag(vext+1/(dximp**2)) + np.diag(vec, k = 1) + np.diag(vec, k = -1)
 #v_KS =(np.divide(np.dot(-Tmat, sqrt_dens0),sqrt_dens0))
-#####################################################################
-#KS Hamiltonian
-#diags = np.array([v_KS+diag, diag/-2, diag/-2])
-s = (x.size,x.size)
-#HS1 = spa.dia_matrix((diags,[0,-1,1]),shape=s)
-#HS2 = spa.kron(HS1,spa.identity(Nx)) + spa.kron(spa.identity(Nx),HS1)
-# Diagonalizations of sparse matrix
-#vals, vecs = eigsh(HS1,which='SA')
-#phi0 = np.matrix(vecs[:,0])
-#KSdens= 2*np.square(vecs[:,0])
-#checking the correctness of the KS potential wrt the imported density
-#plt.plot(x,density, label='GS dens ')
-#plt.plot(x,KSdens, label='KS dens ')
-#plt.legend()
-#plt.show()
-#plt.close()
-#Hartree potential
-#vh = np.convolve(density, 1/np.sqrt(a**2+x**2), mode='same')
 
-def interaction(var):
-    return 1/np.sqrt(a**2+var**2)
-SC2intmat = np.empty((Nx,Nx))
-for i in range(Nx):
-    for j in range(Nx):
-        SC2intmat[i,j]=interaction(x[i]-x[j])
+######################Initial Guess###############################################
+
+
+# 2 body soft-coulomb matrix
+
+SC2intmat = SC2mat(Nx)
+
+# Hartree potnetial
+
 vh = SC2intmat.dot(density)
+
+# Inintial KS eigenfunctions 
+
 diag = np.ones(Nx)/dx**2
 diags = np.array([vh+diag, diag/-2, diag/-2])
 HSguess= spa.dia_matrix((diags,[0,-1,1]),shape=s)
 valsg, vecsg = eigsh(HSguess,which='SA')
-#phi0g = np.array(vecsg[:,0])
-#gdens = 2*np.square(phi0g)
-#phi1g = np.array(vecsg[:,1])
-gdens = (2-gw)*np.square(vecsg[:,0])+gw*np.square(vecsg[:,1])
-#gdens = np.sum(vecsg[:,0:2]**2,axis=1)
-#print(vecsg.shape)
-#print(np.sum(gdens*dx))
 
-#plt.plot(x,density-gdens,label='target-inv')
-#plt.plot(x, gdens, label='inv dens')
-#plt.legend()
-#plt.show()
-#plt.close
+# Initial KS  ensemble density 
+
+gdens = (2-gw)*np.square(vecsg[:,0])+gw*np.square(vecsg[:,1])
+
+# the potential updated is a copy of the hartree potential
 
 number=1
 vinv=np.copy(vh)
-while number > 0.000005:
-    vinv = gdens*vinv/density
-    diags = np.array([vinv+diag, diag/-2, diag/-2])
-    HSguess= spa.dia_matrix((diags,[0,-1,1]),shape=s)
-    valsg, vecsg = eigsh(HSguess,which='SA')
-    #phi0g = np.array(vecsg[:,0])
-    #gdens = 2*np.square(phi0g)
-    gdens = (2-gw)*np.square(vecsg[:,0])+gw*np.square(vecsg[:,1])
-    number=(np.abs(np.ones(Nx)-gdens/density)).max()
-    print(number)
+
+###########################Inversion###################################
+
+
+vinv, valsg, vecsg = Lee_Bar_Inv_Ens(vinv,gdens,density,gw,.0000005)
+
+
+###########################Checking energy difference#################
+
+
 E1ks = valsg[0]+valsg[1]
 E0ks = 2*valsg[0]
-print(valsg[1]-valsg[0])
+print('Eksw1-Eksw0: ',valsg[1]-valsg[0])
 
+
+###########################Plotting###################################
 
  
 #plt.plot(x,density-gdens, label='GS dens-guess dens ')
-plt.plot(x,vinv -vinv[0]-(vh-vh[0]),label='vinv')
+#plt.plot(x,vinv -vinv[0]-(vh-vh[0]),label='vinv')
 #plt.plot(ximp, v_KS-v_KS[0], label='vKS-GS')
-plt.legend()
-plt.show()
-plt.close
+#plt.legend()
+#plt.show()
+#plt.close
 
 #plt.plot(x,gdens,label='init guess 1st')
 #plt.plot(x,density,label='ref 1st')
@@ -109,14 +150,19 @@ plt.close
 #plt.show()
 #plt.close()
 
-plt.plot(x,density-gdens,label='target-inv')
+#plt.plot(x,density-gdens,label='target-inv')
 #plt.plot(x, gdens, label='inv dens')
-plt.legend()
-plt.show()
-plt.close
+#plt.legend()
+#plt.show()
+#plt.close
 
 
-#save vH
-#np.savetxt('vhpy.dat', zip(np.transpose(x),np.transpose(vh)), fmt='%.4e', delimiter=' ')
+########################Saving info###################################
 
+
+#save ensemble KS eigenfunctions, Eigenvalues, and Potential
+
+np.savetxt('vks_bi_w=.125_1000_9.dat', vinv, fmt='%.9e', delimiter=' ')
+np.savetxt('evecs_bi_w=.125_1000_9.dat', vecsg[:,0:2], fmt='%.9e', delimiter=' ')
+np.savetxt('evals_bi_w=.125_1000_9.dat', valsg[0:2], fmt='%.9e', delimiter=' ')
 
